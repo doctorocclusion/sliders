43a1fda7a3a8658025ce6b8efc6e87d2464645d6
Sam Sartor <me@drocclusion.net>
2016-11-03 23:45:52 -0600

    Better Data
    Lots of structs for dealing with cycles

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,39 @@ use time::{PreciseTime};
 use std::io::{self, BufRead};
 
 fn main() {
-	let stdin = io::stdin();
-	loop {
-   		let line = stdin.lock().lines().next().unwrap().unwrap();
-   		let n = line.parse::<i64>().unwrap() as usize;
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
 
-   		let start = PreciseTime::now();
-   		let out = slide::get_permute_order(n);
-   		let start = start.to(PreciseTime::now());
+    loop {
+        let line = lines.next().unwrap().unwrap();
+        let n = line.parse::<i64>().unwrap() as usize;
 
-   		println!("    Done in {} ms", start.num_milliseconds());
-		println!("    Permutation #{} has order {} and {} cycles with lengths:", n, out.0, out.1.len());
-		print!("  ");
-		for c in out.1 {
-			print!("  {} ({}...)", c.1, c.0);
-		}
-		println!("");
-	}
+        let start = PreciseTime::now();
+        let cycles = slide::get_cycles(n);
+        let order = cycles.iter()
+            .map(|ref i| i.get_order())
+            .max()
+            .unwrap();
+        let start = start.to(PreciseTime::now());
+
+        println!("    Done in {} ms", start.num_milliseconds());
+        println!("    Permutation #{} has order {} and {} cycles with lengths:", n, order, cycles.len());
+        for c in cycles.iter().take(100) {
+            let mut cit = c.iter();
+            let mut cbt = c.iter_back();
+            cbt.prev_i();
+            match c.get_order() {
+                1 => println!("        {} ({})", c.get_order(), cit.next_i()),
+                2 => println!("        {} ({}, {})", c.get_order(), cit.next_i(), cit.next_i()),
+                3 => println!("        {} ({}, {}, {})", c.get_order(), cit.next_i(), cit.next_i(), cit.next_i()),
+                4 => println!("        {} ({}, {}, {}, {})", c.get_order(), cit.next_i(), cit.next_i(), cit.next_i(), cit.next_i()),
+                5 => println!("        {} ({}, {}, {}, {}, {})", c.get_order(), cit.next_i(), cit.next_i(), cit.next_i(), cit.next_i(), cit.next_i()),
+                _ => println!("        {} ({}, {}, {}, ..., {5}, {4})", c.get_order(), cit.next_i(), cit.next_i(), cit.next_i(), cbt.prev_i(), cbt.prev_i())
+            }
+        }
+        if (cycles.len() > 100) {
+            println!("        ... ({} more)", cycles.len() - 100); 
+        }
+        println!("");
+    }
 }
--- src/slide.rs
+++ src/slide.rs
@@ -1,99 +1,140 @@
-pub fn slide_permute(i: usize, n: usize) -> usize {
-	if i % 2 == 0 { i / 2 }
-	else { n - (i + 1) / 2 }
+pub use std::iter::{Iterator, Take};
+
+pub struct SlideIter {
+    next_i: usize,
+    n: usize
 }
 
-pub fn get_cycles(n: usize) -> Vec<Vec<usize>> {
-	// list of finished cycles
-	let mut cycles = Vec::new();
+impl SlideIter {
+    pub fn new(i: usize, n: usize) -> SlideIter {
+        SlideIter {
+            next_i: i, 
+            n: n
+        }
+    }
 
-	// what elements have already been assigned to a cycle
-	let mut found = Vec::with_capacity(n);
-	for _ in 0..n {
-		found.push(false);
-	}
+    pub fn next_i(&mut self) -> usize {
+        let out = self.next_i;
+        self.next_i = if self.next_i % 2 == 0 { self.next_i / 2 }
+                      else { self.n - (self.next_i + 1) / 2 };
+        out
+    }
 
-	// the 0 cycle is always its own length 1 cycle
-	cycles.push(vec!(0));
+    pub fn get_deck_size(&self) -> usize { self.n }
 
-	if n == 0 { return cycles; }
-
-	'cycles_loop: for start in 1..n {
-		let mut i = start;
-
-		let mut cycle = vec!(i);
-		found[i] = true;
-
-		loop {
-			i = slide_permute(i, n);
-			if i == start { break; }
-			if found[i] { continue 'cycles_loop; }
-			cycle.push(i);
-			found[i] = true;
-		}
-
-		cycles.push(cycle);
-	}
-
-	cycles
+    pub fn reverse(self) -> SlideBackIter { 
+        let mut out = SlideBackIter::new(self.next_i, self.n);
+        out.next();
+        out
+    }
 }
 
-pub fn get_cycles_info(n: usize) -> Vec<(usize, usize)> {
-	// list of finished cycles
-	let mut cycles = Vec::new();
+impl Iterator for SlideIter {
+    type Item = usize;
 
-	if n == 0 { return cycles; }
-
-	// the 0 cycle is always its own length 1 cycle
-	cycles.push((0, 1));
-
-	// to ensure that our outputted cycles are not duplicated, we will ignore
-	// any cycles that do not start on their lowest number (only one such
-	// ordering of a given cycle can exist)
-
-	// if first is even, then permute(first) < first
-	// if first is odd and greater than 2 * n / 3, then permute(first) < first
-
-	// so we can save time by only starting with the odd numbers less than or equal to 2/3rds n
-	'cycles_loop: for start in 0..(n / 3 + 1) {
-		let start = start * 2 + 1;
-		let mut i = start;
-
-		let mut count = 0 as usize;
-
-		loop {
-			// i is guaranteed to be odd, permute as such
-			i = n - (i + 1) / 2;
-			count += 1;
-
-			// how many times can i be divided by 2? each one of these
-			// divisions is a further step in the cycle
-			let tz = i.trailing_zeros() as usize;
-
-			i >>= tz;
-			// i is now odd by definition
-			count += tz;
-
-			// this ordering of the cycle did not start with its lowest value
-			if i < start { continue 'cycles_loop; }
-
-			// we started with the lowest odd number so we know we did not miss a point when i == start
-			if i == start { break; }
-		}
-
-		cycles.push((start, count));
-	}
-
-	cycles
+    fn next(&mut self) -> Option<usize> { Some(self.next_i()) }
 }
 
-pub fn get_permute_order(n: usize) -> (usize, Vec<(usize, usize)>) {
-	let cycles = get_cycles_info(n);
+pub struct SlideBackIter {
+    next_i: usize,
+    n: usize
+}
 
-	let mut order = 0;
-	for i in 0..cycles.len() {
-		if cycles[i].1 > order { order = cycles[i].1 };
-	}
+impl SlideBackIter {
+    pub fn new(i: usize, n: usize) -> SlideBackIter {
+        SlideBackIter {
+            next_i: i, 
+            n: n
+        }
+    }
 
-	return (order, cycles);
+    pub fn prev_i(&mut self) -> usize {
+        let out = self.next_i;
+        self.next_i = if self.next_i < (self.n + 1) / 2 { self.next_i * 2 }
+                      else { (self.n - self.next_i)  * 2 - 1 };
+        out
+    }
+
+    pub fn get_deck_size(&self) -> usize { self.n }
+
+    pub fn reverse(self) -> SlideIter { 
+        let mut out = SlideIter::new(self.next_i, self.n);
+        out.next();
+        out
+    }
+}
+
+impl Iterator for SlideBackIter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<usize> { Some(self.prev_i()) }
+}
+
+pub struct Cycle {
+    start: usize,
+    order: usize,
+    deck_size: usize
+}
+
+impl Cycle {
+    pub fn new(n: usize, lowest: usize, order: usize) -> Cycle { 
+        Cycle { 
+            start: lowest, 
+            order: order, 
+            deck_size: n
+        }
+    }
+
+    pub fn iter(&self) -> SlideIter { SlideIter::new(self.start, self.deck_size) }
+    pub fn iter_back(&self) -> SlideBackIter { SlideBackIter::new(self.start, self.deck_size) }
+    pub fn iter_once(&self) -> Take<SlideIter> { self.iter().take(self.order) }
+    pub fn iter_back_once(&self) -> Take<SlideBackIter> { self.iter_back().take(self.order) }
+    pub fn get_order(&self) -> usize { self.order }
+    pub fn get_lowest(&self) -> usize { self.start }
+    pub fn get_deck_size(&self) -> usize { self.deck_size }
+}
+
+pub fn get_cycles(n: usize) -> Vec<Cycle> {
+    if n == 0 { return vec![]; }
+
+    // list of finished cycles
+    // the 0 cycle is always its own length 1 cycle
+    let mut cycles = vec![Cycle::new(n, 0, 1)];
+
+    // to ensure that our outputted cycles are not duplicated, we will ignore
+    // any cycles that do not start on their lowest number (only one such
+    // ordering of a given cycle can exist)
+
+    // if first is even, then permute(first) < first
+    // if first is odd and greater than 2 * n / 3, then permute(first) < first
+
+    // so we can save time by only starting with the odd numbers less than or equal to 2/3rds n
+    'cycles_loop: for start in 0..(n / 3 + 1) {
+        let start = start * 2 + 1;
+        let mut i = start;
+
+        let mut count = 0 as usize;
+
+        loop {
+            // i is guaranteed to be odd, permute as such
+            i = n - (i + 1) / 2;
+            count += 1;
+
+            // how many times can i be divided by 2?
+            let tz = i.trailing_zeros() as usize;
+
+            i >>= tz; // divide i by 2 until odd
+            count += tz; // each one of these divisions is a further step in the cycle
+            
+            // this ordering of the cycle did not start with its lowest value
+            if i < start { continue 'cycles_loop; }
+
+            // we started with the lowest odd number so we know we did not miss a point when i == start
+            if i == start { break; }
+        }
+
+        cycles.push(Cycle::new(n, start, count));
+    }
+
+    cycles
 }
